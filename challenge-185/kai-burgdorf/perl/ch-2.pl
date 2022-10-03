6256f8ea457582958de2bb9c4f896df81f1cf172
Kai Burgdorf <k@iburgdorf.de>
2022-10-03 11:22:58 +0200

    solved ch-2.pl

--- challenge-185/kai-burgdorf/perl/ch-2.pl
+++ challenge-185/kai-burgdorf/perl/ch-2.pl
@@ -1,3 +1,29 @@
 #!/usr/bin/env perl
 
-print "test";
+use strict;
+use warnings;
+
+my @list = ('ab-cde-123', '123.abc.420', '3abc-0010.xy');
+my @outlist;
+
+foreach (@list) { push(@outlist, mask_first_four($_)); }
+
+use Data::Dumper;
+print Dumper \@list;
+print Dumper \@outlist;
+
+sub mask_first_four() {
+	my ($str) = @_;
+	my $replacements_left = 4;
+	my @lits = split //, $str;
+
+	foreach my $index (0 .. $#lits) {
+		if ($lits[$index] =~ m/([a-z]|[0-9])/) {
+			$lits[$index] = 'x';
+			$replacements_left--;
+		}
+		last if ($replacements_left < 1);
+	}
+
+	return join("", @lits);
+}
