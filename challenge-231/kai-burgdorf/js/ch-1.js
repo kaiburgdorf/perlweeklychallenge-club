c945e97241991eea918cb89fa6a9f118e8e55b1a
Kai Burgdorf <k@iburgdorf.de>
2023-08-29 22:49:05 +0200

    return -1 if no value is valid

--- challenge-231/kai-burgdorf/js/ch-1.js
+++ challenge-231/kai-burgdorf/js/ch-1.js
@@ -1,14 +1,15 @@
 var input = [3, 2, 1, 4];
+//var input = [3, 2];
+//var input = [2, 1, 3];
 console.log(input);
 
 var min = input[0];
 var max = input[0];
 
 input.forEach((x) => {
-	if(x < min) min = x;
+  if(x < min) min = x;
   if(x > max) max = x;
 });
 
 var output = [...input.filter(x => x != min && x != max)];
-
-console.log(output);
+(output.length > 0) ? console.log(output) : console.log("-1");
